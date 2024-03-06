0487d6d9b2d46f58814395d7e0c29d73e42c9e35
Samuel <adetayo456@gmail.com>
2024-03-06 12:43:12 +0100

    Conditionals (tenary) Operator

--- index.js
+++ index.js
@@ -374,13 +374,13 @@ if (23 > 10) { //statements
   const str = '23 is bigger';
 }
 
-console.log(`I'm $(2037 - 1991) years old.`) //Expression
-console.log(`I'm $(2037 - 1991) years old $(if (23 > 10) {
-  const str = '23 is bigger'; 
-})`)   //Error becos statements don't makes sense where Javascripts expects an expression
+console.log(`I'm ${2037 - 1991} years old.`) //Expression
+//console.log(`I'm ${2037 - 1991} years old ${ if (23 > 10) {
+ // const str = '23 is bigger'; 
+//}}`)   //Error becos statements don't makes sense where Javascripts expects an expression
 
 const me = 'Jones';
-console.log(`I'm $(2037 - 1991) years old ${me}`);
+console.log(`I'm ${2037 - 1991} years old ${me}`);
 
 const age = 23;
 //age >= 18 ? console.log('I like to drink wine 🍷'): //An Operator is an expression becos it always produces a value
